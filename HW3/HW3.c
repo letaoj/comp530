a31d51084741de8742c1f477c118ca2932d9a98c
letaoj <letaoj@gmail.com>
2014-10-30 16:31:41 -0400

    alignment change to HW3

--- HW3/HW3.c
+++ HW3/HW3.c
@@ -24,8 +24,8 @@
 #define CARRIAGE '\r'
 
 typedef struct {
-	buffer *in;
-	buffer *out;
+  buffer *in;
+  buffer *out;
 } ThreadInit;
 
 // Method signature
@@ -35,149 +35,149 @@ void *asterisks_processing_thread(void *state);
 void *output_thread(void *state);
 
 int main() {
-	//malloc all the buffers
-	buffer input_buffer;
-	buffer newline_handler_buffer;
-	buffer asterisk_handler_buffer;
+  //malloc all the buffers
+  buffer input_buffer;
+  buffer newline_handler_buffer;
+  buffer asterisk_handler_buffer;
 
-	// create and initiate the buffers
-	createBuffer(&input_buffer);
-	createBuffer(&newline_handler_buffer);
-	createBuffer(&asterisk_handler_buffer);
+  // create and initiate the buffers
+  createBuffer(&input_buffer);
+  createBuffer(&newline_handler_buffer);
+  createBuffer(&asterisk_handler_buffer);
 
-	// create the initiate state for each thread
-	ThreadInit input_thread_init = { NULL, &input_buffer };
-	ThreadInit carriage_processing_thread_init = { &input_buffer,
-			&newline_handler_buffer };
-	ThreadInit asterisks_processing_thread_init = { &newline_handler_buffer,
-			&asterisk_handler_buffer };
-	ThreadInit output_thread_init = { &asterisk_handler_buffer, NULL };
+  // create the initiate state for each thread
+  ThreadInit input_thread_init = { NULL, &input_buffer };
+  ThreadInit carriage_processing_thread_init = { &input_buffer,
+      &newline_handler_buffer };
+  ThreadInit asterisks_processing_thread_init = { &newline_handler_buffer,
+      &asterisk_handler_buffer };
+  ThreadInit output_thread_init = { &asterisk_handler_buffer, NULL };
 
-	// Initialize the libST runtime.
-	st_init();
+  // Initialize the libST runtime.
+  st_init();
 
-	//create a thread for reading input
-	if (st_thread_create(input_thread, &input_thread_init, 0, 0) == NULL) {
-		perror("failed to create input thread");
-		exit(1);
-	}
+  //create a thread for reading input
+  if (st_thread_create(input_thread, &input_thread_init, 0, 0) == NULL) {
+    perror("failed to create input thread");
+    exit(1);
+  }
 
-	//create a thread for replacing the carriage returns with blanks
-	if (st_thread_create(carriage_processing_thread,
-			&carriage_processing_thread_init, 0, 0) == NULL) {
-		perror("failed to create processing carriage thread");
-		exit(1);
-	}
+  //create a thread for replacing the carriage returns with blanks
+  if (st_thread_create(carriage_processing_thread,
+                       &carriage_processing_thread_init, 0, 0) == NULL) {
+    perror("failed to create processing carriage thread");
+    exit(1);
+  }
 
-	//create a thread for processing asterisks
-	if (st_thread_create(asterisks_processing_thread,
-			&asterisks_processing_thread_init, 0, 0) == NULL) {
-		perror("failed to create processing asterisks thread");
-		exit(1);
-	}
+  //create a thread for processing asterisks
+  if (st_thread_create(asterisks_processing_thread,
+                       &asterisks_processing_thread_init, 0, 0) == NULL) {
+    perror("failed to create processing asterisks thread");
+    exit(1);
+  }
 
-	//create a thread for writing output
-	if (st_thread_create(output_thread, &output_thread_init, 0, 0) == NULL) {
-		perror("failed to create output thread");
-		exit(1);
-	}
+  //create a thread for writing output
+  if (st_thread_create(output_thread, &output_thread_init, 0, 0) == NULL) {
+    perror("failed to create output thread");
+    exit(1);
+  }
 
-	// Exit from main via ST.
-	st_thread_exit(NULL);
+  // Exit from main via ST.
+  st_thread_exit(NULL);
 
-	return 0;
+  return 0;
 }
 
 /*
  *  Take the input from standard input and put them into the input buffer
  */
 void *input_thread(void *state) {
-	ThreadInit *init = state;
-	char c;
+  ThreadInit *init = state;
+  char c;
 
-	while ((c = getchar()) != EOF) {
-		deposit(init->out, c);
-	}
-	deposit(init->out, c);
-	// Exit from main via ST.
-	st_thread_exit(NULL);
+  while ((c = getchar()) != EOF) {
+    deposit(init->out, c);
+  }
+  deposit(init->out, c);
+  // Exit from main via ST.
+  st_thread_exit(NULL);
 
-	// get rid of the warning
-	return NULL;
+  // get rid of the warning
+  return NULL;
 }
 
 /*
  * process the input and replace all the carriage to blank
  */
 void *carriage_processing_thread(void *state) {
-	ThreadInit *init = (ThreadInit *) state;
-	char c;
-	while ((c = remoove(init->in)) != EOF) {
-		if (c == NEWLINE || c == CARRIAGE) {
-			c = BLANK;
-		}
-		deposit(init->out, c);
-	}
-	deposit(init->out, c);
-	// Exit from main via ST.
-	st_thread_exit(NULL);
+  ThreadInit *init = (ThreadInit *) state;
+  char c;
+  while ((c = remoove(init->in)) != EOF) {
+    if (c == NEWLINE || c == CARRIAGE) {
+      c = BLANK;
+    }
+    deposit(init->out, c);
+  }
+  deposit(init->out, c);
+  // Exit from main via ST.
+  st_thread_exit(NULL);
 
-	// get rid of the warning
-	return NULL;
+  // get rid of the warning
+  return NULL;
 }
 
 /*
  * process the input and handler the *.
  */
 void *asterisks_processing_thread(void *state) {
-	ThreadInit *init = state;
-	char c;
+  ThreadInit *init = state;
+  char c;
 
-	while ((c = remoove(init->in)) != EOF) {
-		if (c == ASTERISK) {
-			c = remoove(init->in);
-			if (c != ASTERISK) {
-				deposit(init->out, ASTERISK);
-				deposit(init->out, c);
-			} else {
-				deposit(init->out, CARET);
-			}
-			if (c == EOF) {
-				st_thread_exit(NULL);
-			}
-		} else {
-			deposit(init->out, c);
-		}
-	}
-	deposit(init->out, c);
-	// Exit from main via ST.
-	st_thread_exit(NULL);
+  while ((c = remoove(init->in)) != EOF) {
+    if (c == ASTERISK) {
+      c = remoove(init->in);
+      if (c != ASTERISK) {
+        deposit(init->out, ASTERISK);
+        deposit(init->out, c);
+      } else {
+        deposit(init->out, CARET);
+      }
+      if (c == EOF) {
+        st_thread_exit(NULL);
+      }
+    } else {
+      deposit(init->out, c);
+    }
+  }
+  deposit(init->out, c);
+  // Exit from main via ST.
+  st_thread_exit(NULL);
 
-	// get rid of the warning
-	return NULL;
+  // get rid of the warning
+  return NULL;
 }
 
 /*
  * Output to the standard output
  */
 void *output_thread(void *state) {
-	ThreadInit *init = state;
-	int count = 0;
-	char c;
-	char line[MAX_LINE_SIZE + 1];
+  ThreadInit *init = state;
+  int count = 0;
+  char c;
+  char line[MAX_LINE_SIZE + 1];
 
-	while ((c = remoove(init->in)) != EOF) {
-		line[count++] = c;
-		if (count == MAX_LINE_SIZE) {
-			line[count] = '\0';
-			printf("%s\n", line);
-			count = 0;
-		}
-	}
+  while ((c = remoove(init->in)) != EOF) {
+    line[count++] = c;
+    if (count == MAX_LINE_SIZE) {
+      line[count] = '\0';
+      printf("%s\n", line);
+      count = 0;
+    }
+  }
 
-	// Exit from main via ST and free pointer.
-	st_thread_exit(NULL);
+  // Exit from main via ST and free pointer.
+  st_thread_exit(NULL);
 
-	// get rid of the warning
-	return NULL;
+  // get rid of the warning
+  return NULL;
 }
